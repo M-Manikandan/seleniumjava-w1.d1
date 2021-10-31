6f68ed7a8c8fe94684057025cad019027f476225
M-Manikandan <93439317+M-Manikandan@users.noreply.github.com>
2021-10-31 08:13:06 +0530

    Add files via upload

---
+++ ConvertNegativeToPositive.java
@@ -0,0 +1,17 @@
+package week1.day1;
+
+public class ConvertNegativeToPositive {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int number = -40;
+		int x;
+		if (number < 0) {
+			x = number * (-1);
+			System.out.println("Positive number :" + x);
+			System.out.println("The number -40 is converted to 40");
+		}
+
+	}
+
+}
---
+++ LearnIfStatement.java
@@ -0,0 +1,19 @@
+package week1.day1;
+
+public class LearnIfStatement {
+public static void main(String[] args) {
+	
+	int number = 35; 
+	if (number > 0) {
+		System.out.println("The number is Positive");
+				
+	}
+	else if (number < 0) {
+		System.out.println("The number is Negative");
+	}
+	else
+	{
+		System.out.println("The number is neither Positive nor Negative");
+	}
+}
+}
